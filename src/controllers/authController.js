ee20bc47652c83d89d48c50e4bd9d07601781de3
hubertfus <hubertfusiarz@gmail.com>
2025-07-20 11:29:40 +0200

    refactor: replace pg with Prisma

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,11 @@
-import { createUser, getUserByEmail } from "../models/userModel.js";
+import prisma from "../models/userModel.js";
 import bcrypt from "bcrypt";
 
 export const register = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        const existingUser = await getUserByEmail(email);
+        const existingUser = await prisma.user.findUnique({ where: { email } });
         if (existingUser) {
             return res.status(409).json({
                 messages: [
@@ -20,14 +20,13 @@ export const register = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const newUser = await createUser({
-            ...req.body,
-            password: hashedPassword,
+        const newUser = await prisma.user.create({
+            data: { ...req.body, password: hashedPassword },
         });
 
         res.status(201).json({
             message: "User registered successfully",
-            user: newUser,
+            user: { id: newUser.id, email: newUser.email },
         });
     } catch (error) {
         console.error("Registration error:", error);
